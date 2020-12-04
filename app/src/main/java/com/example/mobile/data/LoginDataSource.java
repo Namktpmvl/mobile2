0a5f1e3dca0b94dd41d3473774a4b2992c5e45a4
yolochocopie <chocopieyolo@gmail.com>
2020-12-05 00:54:56 +0700

    test2

--- app/src/main/java/com/example/mobile/data/LoginDataSource.java
+++ app/src/main/java/com/example/mobile/data/LoginDataSource.java
@@ -8,7 +8,7 @@ import java.io.IOException;
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
-
+/** test */
     public Result<LoggedInUser> login(String username, String password) {
 
         try {
@@ -18,8 +18,6 @@ public class LoginDataSource {
                             java.util.UUID.randomUUID().toString(),
                             "Jane Doe");
             return new Result.Success<>(fakeUser);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
         }
     }
 
